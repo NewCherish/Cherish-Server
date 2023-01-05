f21d7f08a2a0a2e3e09820e93424125fe4b1f4b0
Chae Jeong Ah <jokj624@gmail.com>
2023-01-05 16:03:00 +0900

    Setting: swagger 세팅 (#7)
    * Feat: prettier 세팅
    
    * Feat: swagger 세팅
    
    * Chore: swagger package 설치

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setUpSwagger } from './config/swagger';
 
 require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setUpSwagger(app);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
