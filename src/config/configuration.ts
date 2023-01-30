d4c01d478614fbd10badaadf5a47a7288a55f637
jokj624 <jokj624@gmail.com>
2023-01-30 20:57:30 +0900

    Feat: access/refresh secret 환경변수 추가

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,4 +4,6 @@ export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   sentryWebhookUrl: process.env.SLACK_WEBHOOK_URL,
   sentryDsn: process.env.SENTRY_DSN,
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
 });
