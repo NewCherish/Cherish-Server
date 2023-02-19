e8db2504fc7642edc36347a249ee37d958f9e9ee
jokj624 <jokj624@gmail.com>
2023-02-19 19:54:42 +0900

    Feat: user service 로직

---
+++ src/users/users.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService, PrismaService],
+  exports: [UsersService],
+})
+export class UsersModule {}
