d67d70390f6490a23fe3f245de8613989bbd1908
Chae Jeong Ah <jokj624@gmail.com>
2023-03-11 18:02:09 +0900

    Fix: resolve conflict, build fail and add .env to develop.yml (#17)
    * Chore: auth 필요한 모듈 설치
    
    * Fix: class-transformer 설정
    
    * Feat: kakao login controller, service 로직
    
    * Feat: base dto, interface
    
    * Feat: user service 로직
    
    * Feat: error, validation util 작성
    
    * Feat: exception directory export
    
    * Refactor: 불필요한 error base dto 제거
    
    * Refactor: error util 함수 내 customError 함수 작성
    
    * Refactor: 불필요한 import 제거
    
    * Fix: change prisma client path

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Injectable, Logger } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
