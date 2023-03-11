2c9685ef10642a66fad632b8e17e9db38d6b9f95
seohyun <61549796+seohyun-106@users.noreply.github.com>
2023-03-11 19:31:40 +0900

    Feat: plants 뼈대 파일 (#22)
    * Feat: plants 틀
    
    * Feat: Plants 틀
    
    * Fix: plants.module import 추가

--- src/plants/plants.module.ts
+++ src/plants/plants.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PlantsService } from './plants.service';
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { Validation } from 'src/utils/validation';
 import { PlantsController } from './plants.controller';
 
 @Module({
+  imports: [ConfigModule]
   controllers: [PlantsController],
   providers: [PlantsService, PrismaService, ConfigService, Validation]
 })
