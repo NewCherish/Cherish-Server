67177f8ce376332365425dbbd1347151a5c69c36
seohyun-106 <changseohyun@sookmyung.ac.kr>
2023-03-11 19:28:49 +0900

    Fix: plants.module import 추가

--- src/plants/plants.module.ts
+++ src/plants/plants.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PlantsService } from './plants.service';
+import { PrismaService } from 'src/prisma.service';
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { Validation } from 'src/utils/validation';
+
 import { PlantsController } from './plants.controller';
 
 @Module({
+  imports: [ConfigModule]
   controllers: [PlantsController],
   providers: [PlantsService, PrismaService, ConfigService, Validation]
 })
