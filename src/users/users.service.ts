d638efc23868308553f7f228ac14e35432db5f51
jokj624 <jokj624@gmail.com>
2023-07-16 00:47:25 +0900

    Refactor: try-catch 제거, constants 사용

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,70 +1,46 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { CustomException } from 'src/exceptions';
 import { PrismaService } from 'src/prisma.service';
-import { internalServerError } from 'src/utils/error';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  private readonly logger = new Logger(UsersService.name);
-
-  async createUser(newUser): Promise<User | CustomException> {
-    try {
-      const user = await this.prisma.user.create({ data: newUser });
-      return user;
-    } catch (error) {
-      this.logger.error({ error });
-      return internalServerError();
-    }
+  async createUser(newUser): Promise<User> {
+    const user = await this.prisma.user.create({ data: newUser });
+    return user;
   }
 
-  async getUserById(id: number): Promise<User | CustomException> {
-    try {
-      const user = await this.prisma.user.findUnique({
-        where: {
-          id,
-          isDeleted: false,
-        },
-      });
-      return user;
-    } catch (error) {
-      this.logger.error({ error });
-      return internalServerError();
-    }
+  async getUserById(id: number): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id,
+        isDeleted: false,
+      },
+    });
+    return user;
   }
 
-  async getUserByKaKaoId(kakaoId: number): Promise<User | CustomException> {
-    try {
-      const user = await this.prisma.user.findFirst({
-        where: {
-          kakaoId,
-          isDeleted: false,
-        },
-      });
-      return user;
-    } catch (error) {
-      this.logger.error({ error });
-      return internalServerError();
-    }
+  async getUserByKaKaoId(kakaoId: number): Promise<User> {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        kakaoId,
+        isDeleted: false,
+      },
+    });
+    return user;
   }
 
   async updateRefreshTokenByUserId(
     id: number,
     refreshToken: string,
-  ): Promise<User | CustomException> {
-    try {
-      const updatedUser = await this.prisma.user.update({
-        where: { id },
-        data: {
-          refreshToken,
-        },
-      });
-      return updatedUser;
-    } catch (error) {
-      this.logger.error(error);
-      return internalServerError();
-    }
+  ): Promise<User> {
+    const updatedUser = await this.prisma.user.update({
+      where: { id },
+      data: {
+        refreshToken,
+      },
+    });
+    return updatedUser;
   }
 }
