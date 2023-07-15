5c77936b64dde6c0e174bbcdb4074299123c3862
jokj624 <jokj624@gmail.com>
2023-07-16 00:48:27 +0900

    Feat: plants swagger constants 추가

---
+++ src/constants/swagger/index.ts
@@ -0,0 +1,8 @@
+import { SIGNIN_DESCRIPTION } from './auth';
+import { PLANT_INFORMATION } from './plants';
+
+export const ERROR_DESCRIPTION = {
+  INTERNAL_SERVER_ERROR: 'Internal Server Error',
+};
+
+export { SIGNIN_DESCRIPTION, PLANT_INFORMATION };
