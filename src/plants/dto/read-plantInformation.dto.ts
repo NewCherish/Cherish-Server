54a4152d0ffb8812b52c34dbc211020c8abf937d
namjiyun <namjiyun3275@gmail.com>
2023-07-22 19:58:29 +0900

    Chore: information param dto 삭제

--- src/plants/dto/read-plantInformation.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Type } from 'class-transformer';
-import { IsNumber } from 'class-validator';
-
-export class ReadPlantInformationParam {
-  @IsNumber()
-  @Type(() => Number)
-  readonly id: number;
-}
