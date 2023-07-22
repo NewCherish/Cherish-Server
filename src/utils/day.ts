3074bc0e32a42124191f1f817bc1a00905a8b4d0
jokj624 <jokj624@gmail.com>
2023-07-22 20:41:35 +0900

    Feat: d-day 계산 함수

---
+++ src/utils/day.ts
@@ -0,0 +1,10 @@
+import * as dayjs from 'dayjs';
+
+export const caculateDday = (today: Date, targetDay: Date) => {
+  const _today = dayjs(today);
+  const _targetDay = dayjs(targetDay);
+
+  const result = _targetDay.diff(_today, 'day', true);
+
+  return Math.floor(result);
+};
