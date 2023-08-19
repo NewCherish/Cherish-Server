3818ed662ba82abc8a17e45e3ae94c17b7e14ebf
jokj624 <jokj624@gmail.com>
2023-08-19 16:47:06 +0900

    Feat: plants e2e test

--- test/plants.e2e-spec.ts
+++ test/plants.e2e-spec.ts
@@ -1,6 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
+import * as dayjs from 'dayjs';
+import * as utc from 'dayjs/plugin/utc';
+import * as timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 import { AppModule } from '../src/app.module';
 import { PlantsModule } from 'src/plants/plants.module';
@@ -10,6 +16,7 @@ import {
   mockNotFoundResponse,
   mockPlantWaterLogsResponse,
   mockPlantsInformationResponse,
+  mockUserPlantDetailSuccessResponse,
 } from './mock/plants.mock';
 
 describe('Plants (e2e)', () => {
@@ -29,6 +36,38 @@ describe('Plants (e2e)', () => {
 
   afterAll(async () => {
     await app.close();
+  });
+
+  describe('[GET] /plants/:id', () => {
+    beforeAll(() => {
+      jest.useFakeTimers({ doNotFake: ['nextTick', 'setImmediate'] });
+      jest.setSystemTime(new Date('2023-08-13 18:00'));
+    });
+
+    afterAll(() => {
+      jest.useRealTimers();
+    });
+
+    it('200 OK', () => {
+      return request(app.getHttpServer())
+        .get(`/plants/1`)
+        .expect(200)
+        .expect(mockUserPlantDetailSuccessResponse);
+    });
+
+    it('400 Bad Request', () => {
+      return request(app.getHttpServer())
+        .get(`/plants/hi`)
+        .expect(400)
+        .expect(mockBadRequestResponse);
+    });
+
+    it('404 Not Found', () => {
+      return request(app.getHttpServer())
+        .get(`/plants/100`)
+        .expect(404)
+        .expect(mockNotFoundResponse);
+    });
   });
 
   describe('[GET] /plants/:id/information', () => {
